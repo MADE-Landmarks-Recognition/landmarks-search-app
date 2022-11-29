92613278ac7ef3582efef2fd271a86144c3ffed6
Aydar Akhmetov <akhmet.btc@yandex.ru>
2022-11-29 21:05:29 +0300

    v0.2

--- app_utils.py
+++ app_utils.py
@@ -6,6 +6,7 @@ import yaml
 from pytorch_metric_learning.utils.accuracy_calculator import AccuracyCalculator, precision_at_k
 from omegaconf.dictconfig import DictConfig
 from torch.utils.data import DataLoader
+import faiss
 
 from retrieval.engine.evaluate import evaluate, get_tester
 from retrieval.models.net import RetrievalNet
@@ -13,7 +14,9 @@ from retrieval.getter import Getter
 
 
 DEVICE = "cuda"
+EMB_SIZE = 384
 CKPT_PATH = Path("./checkpoints/deit_roadmap.ckpt")
+FAISS_PATH = Path("./checkpoints/index.faiss")
 getter = Getter()
 
 
@@ -33,7 +36,16 @@ def get_model():
 
 
 def get_faiss_index():
-    pass
+    # dump example:
+    # index = faiss.IndexFlatL2(512)
+    # index2 = faiss.IndexIDMap(index)
+    # index2.add_with_ids(train_embeddings.numpy(), train_targets.cpu().numpy())
+    # faiss.write_index(index, FAISS_PATH)
+
+    # load index:
+    faiss_index = faiss.IndexFlatL2(EMB_SIZE)
+    faiss_index = faiss.read_index(FAISS_PATH)
+    return faiss_index
 
 
 def get_mapping():
